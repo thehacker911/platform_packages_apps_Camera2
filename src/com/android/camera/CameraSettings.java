CyanogenMod/android_packages_apps_Camera

camera: allow setting video-size before recording starts
Needed for some samsung cameras.

Change-Id: Ib87071bdd900aa1a42253bf6e2038b0af51f065b

src/com/android/camera/CameraSettings.java
@@ -572,6 +572,18 @@ public class CameraSettings {
         }
     }
 
+    /**
+     * Set video size for certain cameras.
+     *
+     * @param params
+     * @param profile
+     */
+    public static void setEarlyVideoSize(Parameters params, CamcorderProfile profile) {
+        if (Util.needsEarlyVideoSize()) {
+            params.set("video-size", profile.videoFrameWidth + "x" + profile.videoFrameHeight);
+        }
+    }
+
     private void initVideoEffect(PreferenceGroup group, ListPreference videoEffect) {
         CharSequence[] values = videoEffect.getEntryValues();
 

thehacker911/platform_packages_apps_Camera2

Change-Id: Ia0f5716d5e6815d249040b08313482a103a36863

src/com/android/camera/CameraSettings.java
@@ -514,4 +514,16 @@ public class CameraSettings {
             params.set("cam-mode", on ? "1" : "0");
         }
     }
+
+    /**
+     * Set video size for certain cameras.
+     *
+     * @param params
+     * @param profile
+     */
+    public static void setEarlyVideoSize(Parameters params, CamcorderProfile profile) {
+        if (CameraUtil.needsEarlyVideoSize()) {
+            params.set("video-size", profile.videoFrameWidth + "x" + profile.videoFrameHeight);
+        }
+    }
 }